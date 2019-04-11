599fa4be3abe887e8ae1575dbb8c9a258150cc43
ChanceMaker <cedrickbaker7@gmail.com>
2019-04-11 02:39:27 -0400

    Finished rotateListRight from Leetcode Assignment Working

--- rotateListRight/main.cpp
+++ rotateListRight/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 /**
  * ListNode created to build a list
  */
@@ -34,6 +35,14 @@ void printVector(std::vector<int> &x){
         std::cout << *itr << " ";
     }
     std::cout << "\n";
+}
+std::vector<int> createVect(std::string x){
+    std::vector<int>  ans;
+    for (int i = 0; i < x.size(); i++) {
+        ans.push_back(x.at(i) - '0');
+
+    }
+    return ans;
 }
 /**
  * @brief printList is used to to print the Listnode list in order
@@ -72,13 +81,78 @@ int listSize(ListNode* head){
  */
 class Solution {
 public:
-    ListNode* rotateRight(ListNode* head, int k) {
+    /**
+     * Rotate the list to the right k number of times.
+     * @param head
+     * This is the head of the initial list
+     * @param k
+     * This is the number of times that you must rotate the list
+     * @return
+     * We will return a new list That is rotated the correct amount of times.
+     */
+    ListNode* rotateRight(ListNode* head, int k) ;
+    ListNode* rotateHelp(ListNode* head);
 
-    }
 };
+/**
+ * This particular function actually uses a rotate left to perform the equivalent of what the rotate right
+ * number of time would be resulting in the same linkedlist. This is done because it is simply easier
+ * to perform a rotate left
+ * @param head
+ * Head of the LinkedList used as a reference pointer to where our linked list starts
+ * @param k
+ * this parameter is simply the number of right rotations of the list that will be done.
+ * @return
+ * It returns a new Linked List that represents the resultant k right shifted list.
+ */
+ListNode* Solution::rotateRight(ListNode *head, int k) {
+    if(!head)
+        return NULL;
+    ListNode * newHead;
+    newHead = head;
+    int size = listSize(newHead);
+    int limit = 0;
+    k %= size;
+    if(k == 0) return head;
+    for(int i = 0; i < size - k; i++){
+        newHead = rotateHelp(newHead);
+    }
+
+    return newHead;
+}
+/**
+ * This function is a helper function to the rotateRight function it actually just performs one shift left of the
+ * Linked List.
+ * @param head
+ * A reference to the head of the list. That is used as a pointer to the front of the list
+ * @param val
+ * This val parameter is just
+ * @return
+ */
+ListNode * Solution::rotateHelp(ListNode *head) {
+    ListNode * newHead;
+    ListNode * current;
+    newHead = head;
+    current = head;
+    int val = head->val;
+    while (current != NULL) {
+        if(current->next != NULL)
+            current->val = current ->next->val;
+        else
+            current->val = val;
+
+        current = current->next;
+    }
+    return newHead;
+}
 int main() {
     Solution answer;
-
-    answer.rotateRight()
+    ListNode * ListA;
+    ListNode * ListB;
+    std::vector<int> Vect1;
+    Vect1 = createVect("12345");
+    ListA = createList(Vect1);
+    ListB = answer.rotateRight(ListA,2);
+    printList(ListB);
     return 0;
 }
