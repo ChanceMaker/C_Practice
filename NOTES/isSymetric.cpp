590275e1094795e8481d108d78d64c386cb82499
ChanceMaker <cedrickbaker7@gmail.com>
2019-04-19 01:19:00 -0400

    working symetric tree check

---
+++ NOTES/isSymetric.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+          helper(root,root);
+    }
+
+    bool helper(TreeNode* R1, TreeNode* R2){
+    	if(R1 == NULL && R2 == NULL)
+    		return true;
+    	if(R1 == NULL || R2 == NULL)
+    		return false;
+    	if(R1->val == R2->val)
+    		return true;
+    	if(R1->val != R2->val)
+    		return false;
+    	return helper(R1->left,R2->left) && helper(R1->right,R2->right);
+
+    }
+};
